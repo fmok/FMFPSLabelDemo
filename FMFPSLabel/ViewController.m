10c3ae277c473829401c44f0394b3f6fe0104fcd
fm <wangjiuyin@jiemian.com>
2017-05-05 15:31:54 +0800

    init

--- FMFPSLabel/ViewController.m
+++ FMFPSLabel/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
+#import "YYFPSLabel.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *myTableView;
+@property (nonatomic, strong) YYFPSLabel *fpsLabel;
 
 @end
 
@@ -17,8 +22,76 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.myTableView];
+    [self.view addSubview:self.fpsLabel];
+    [self.myTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
 }
 
+- (void)updateViewConstraints
+{
+    __weak typeof(self) weakSelf = self;
+    [self.myTableView mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(weakSelf.view);
+    }];
+    [self.fpsLabel mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(weakSelf.view);
+        make.size.mas_equalTo(CGSizeMake(100, 50));
+    }];
+    [super updateViewConstraints];
+}
+
+#pragma mark - UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 40.f;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ViewController *vc = [[ViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"   %@ - %@", @(indexPath.section),@(indexPath.row+1)];
+    cell.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.f green:arc4random_uniform(255)/255.f blue:arc4random_uniform(255)/255.f alpha:1];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+
+#pragma mark - getter & setter
+- (UITableView *)myTableView
+{
+    if (!_myTableView) {
+        _myTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+        _myTableView.backgroundColor = [UIColor whiteColor];
+        _myTableView.delegate = self;
+        _myTableView.dataSource = self;
+    }
+    return _myTableView;
+}
+
+- (YYFPSLabel *)fpsLabel
+{
+    if (!_fpsLabel) {
+        _fpsLabel = [[YYFPSLabel alloc] init];
+    }
+    return _fpsLabel;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
